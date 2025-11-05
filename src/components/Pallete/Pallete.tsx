6f4c84a8f757a6ff240fa24f82262f1976e4eff8
dannguyen8189 <dannguyen8189@gmail.com>
2025-11-04 16:19:37 -0800

    added coolors.co url redirect functionality

--- src/components/Pallete/Pallete.tsx
+++ src/components/Pallete/Pallete.tsx
@@ -8,9 +8,16 @@ type PaletteProps = {
 
 export const Palette: React.FC<PaletteProps> = ({ Pins }) => {
 
+    function getCoolorsUrl(pins: ImagePin[]): string {
+        const baseUrl = 'https://coolors.co/';
+        const hexColors = pins.map(pin => rgbToHex(pin.color).replace('#', ''));
+        return `${baseUrl}${hexColors.join('-')}`;
+    }
 
     return (
         <div>
+            <p>Open your pallete in Coolors!</p>
+            <p><a href={getCoolorsUrl(Pins)} target="_blank" rel="noopener noreferrer">View Palette</a></p>
             {Pins.map((pin) => (
                 <div key={pin.id} style={{ backgroundColor: rgbToString(pin.color), width: '100px', height: '100px', margin: '5px' }}>
                     <p>{rgbToString(pin.color)}</p>
